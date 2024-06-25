28d807ec190ab6c0b28e56aa7ea340c714ea0537
Lucas Klug Lach <167248764+Lucas-Klug-Lach@users.noreply.github.com>
2024-06-25 14:28:17 -0300

    update Java

---
+++ exercicios/HoraDoSistema/src/main/java/com/mycompany/horadosistema/HoraDoSistema.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Casa
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+       Date relogio = new Date();
+        System.out.println("A hora do Sistema é");
+        System.out.println(relogio.toString());
+    }
+}
